6ed74a06bd3030de6fcbc3f8f75dd8ee34345254
Chai <7232280+Chailotl@users.noreply.github.com>
2024-06-10 00:01:43 -0400

    Struct test

--- src/testmod/java/com/chailotl/testmod/ConfigModel.java
+++ src/testmod/java/com/chailotl/testmod/ConfigModel.java
@@ -2,6 +2,7 @@ package com.chailotl.testmod;
 
 import com.chailotl.sushi_bar.owo.config.SushiModmenu;
 import io.wispforest.owo.config.annotation.Config;
+import io.wispforest.owo.config.annotation.Nest;
 import net.minecraft.util.Identifier;
 
 import java.util.Arrays;
@@ -18,4 +19,16 @@ public class ConfigModel
 		new Identifier("minecraft:dripstone_caves"),
 		new Identifier("minecraft:deep_dark")
 	);
+
+	@Nest
+	public Struct struct = new Struct();
+
+	public static class Struct
+	{
+		public List<Identifier> ids = Arrays.asList(
+			new Identifier("minecraft:lush_caves"),
+			new Identifier("minecraft:dripstone_caves"),
+			new Identifier("minecraft:deep_dark")
+		);
+	}
 }
